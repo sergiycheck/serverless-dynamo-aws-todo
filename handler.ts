616618fa3c0ce3a181a22356227b5c151d6fe4db
Serhii Kuzmych <serhii.kuzmych@kapuafrica.com>
2024-02-13 18:18:40 +0200

    feat: init the project

---
+++ handler.ts
@@ -0,0 +1,6 @@
+import { create } from "./todos/create";
+import { get } from "./todos/get";
+import { list } from "./todos/list";
+import { update } from "./todos/update";
+
+export { create, get, list, update };
